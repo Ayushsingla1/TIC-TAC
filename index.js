8de01c513ce2c29babba782ea70d2748878c7664
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2024-03-31 00:10:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let elements=document.querySelectorAll('.box');
 let content=document.querySelector('.result');
 let count=0;
 let style='X';
+let end=0;
 function cross(event){
     if(event.target.textContent=='X' || event.target.textContent=='O'){
         return;
@@ -29,34 +30,42 @@ function cross(event){
         if(element1.textContent==element2.textContent && element2.textContent==element3.textContent){
             if(element1.textContent=='') return;
             content.textContent=`${style}-loss`;
+            end=1;
         }
         else if(element1.textContent==element4.textContent && element4.textContent==element7.textContent){
             if(element1.textContent=='') return;
             content.textContent=`${style}-loss`;
+            end=1;
         }
         else if(element1.textContent==element5.textContent && element5.textContent==element9.textContent){
             if(element1.textContent=='') return;
             content.textContent=`${style}-loss`;
+            end=1;
         }
         else if(element2.textContent==element5.textContent && element5.textContent==element8.textContent){
             if(element2.textContent=='') return;
             content.textContent=`${style}-loss`;
+            end=1;
         } 
         else if(element3.textContent==element6.textContent && element6.textContent==element9.textContent){
             if(element3.textContent=='') return;
             content.textContent=`${style}-loss`;
+            end=1;
         }
         else if(element3.textContent==element5.textContent && element5.textContent==element7.textContent){
             if(element3.textContent=='') return;
             content.textContent=`${style}-loss`;
+            end=1;
         }
         else if(element4.textContent==element5.textContent && element5.textContent==element6.textContent){
             if(element4.textContent=='') return;
             content.textContent=`${style}-loss`;
+            end=1;
         }
         else if(element7.textContent==element8.textContent && element8.textContent==element9.textContent){
             if(element7.textContent=='') return;
             content.textContent=`${style}-loss`;
+            end=1;
         }
         else{
             if(count==9){
@@ -68,6 +77,9 @@ function cross(event){
     }
 elements.forEach(element => {
     element.addEventListener('click',(event)=>{
+        if(end==0)
         cross(event);
+        else
+            console.alert("Game finished");
     })
 });
